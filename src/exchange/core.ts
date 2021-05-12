33e2b4bc89109a98cd7a1809583a3d7284556b2f
Moonster <Moonster.bsc@gmail.com>
2021-05-12 14:06:00 -0400

    chore: Remove mint burn and pairhourdata

--- src/exchange/core.ts
+++ src/exchange/core.ts
@@ -1,165 +1,18 @@
 /* eslint-disable prefer-const */
-import { BigInt, BigDecimal, store } from "@graphprotocol/graph-ts";
+import { BigInt, BigDecimal } from "@graphprotocol/graph-ts";
 import {
   Pair,
   Token,
   PancakeFactory,
   Transaction,
-  Mint as MintEvent,
   Burn as BurnEvent,
   Swap as SwapEvent,
   Bundle,
 } from "../../generated/schema";
-import { Mint, Burn, Swap, Transfer, Sync } from "../../generated/templates/Pair/Pair";
+import { Mint, Burn, Swap, Sync } from "../../generated/templates/Pair/Pair";
 import { updatePairDayData, updateTokenDayData, updatePancakeDayData, updatePairHourData } from "./dayUpdates";
 import { getBnbPriceInUSD, findBnbPerToken, getTrackedVolumeUSD, getTrackedLiquidityUSD } from "./pricing";
-import { convertTokenToDecimal, ADDRESS_ZERO, FACTORY_ADDRESS, ONE_BI, ZERO_BD, BI_18 } from "./utils";
-
-function isCompleteMint(mintId: string): boolean {
-  return MintEvent.load(mintId).sender !== null; // sufficient checks
-}
-
-export function handleTransfer(event: Transfer): void {
-  // Initial liquidity.
-  if (event.params.to.toHex() == ADDRESS_ZERO && event.params.value.equals(BigInt.fromI32(1000))) {
-    return;
-  }
-
-  // get pair and load contract
-  let pair = Pair.load(event.address.toHex());
-
-  // liquidity token amount being transferred
-  let value = convertTokenToDecimal(event.params.value, BI_18);
-
-  // get or create transaction
-  let transaction = Transaction.load(event.transaction.hash.toHex());
-  if (transaction === null) {
-    transaction = new Transaction(event.transaction.hash.toHex());
-    transaction.block = event.block.number;
-    transaction.timestamp = event.block.timestamp;
-    transaction.mints = [];
-    transaction.burns = [];
-    transaction.swaps = [];
-  }
-
-  // mints
-  let mints = transaction.mints;
-  if (event.params.from.toHex() == ADDRESS_ZERO) {
-    // update total supply
-    pair.totalSupply = pair.totalSupply.plus(value);
-    pair.save();
-
-    // create new mint if no mints so far or if last one is done already
-    if (mints.length === 0 || isCompleteMint(mints[mints.length - 1])) {
-      let mint = new MintEvent(
-        event.transaction.hash.toHex().concat("-").concat(BigInt.fromI32(mints.length).toString())
-      );
-      mint.transaction = transaction.id;
-      mint.pair = pair.id;
-      mint.to = event.params.to;
-      mint.liquidity = value;
-      mint.timestamp = transaction.timestamp;
-      mint.transaction = transaction.id;
-      mint.save();
-
-      // update mints in transaction
-      transaction.mints = mints.concat([mint.id]);
-
-      // save entities
-      transaction.save();
-    }
-  }
-
-  // case where direct send first on BNB withdrawals
-  if (event.params.to.toHex() == pair.id) {
-    let burns = transaction.burns;
-    let burn = new BurnEvent(
-      event.transaction.hash.toHex().concat("-").concat(BigInt.fromI32(burns.length).toString())
-    );
-    burn.transaction = transaction.id;
-    burn.pair = pair.id;
-    burn.liquidity = value;
-    burn.timestamp = transaction.timestamp;
-    burn.to = event.params.to;
-    burn.sender = event.params.from;
-    burn.needsComplete = true;
-    burn.transaction = transaction.id;
-    burn.save();
-
-    // TODO: Consider using .concat() for handling array updates to protect
-    // against unintended side effects for other code paths.
-    burns.push(burn.id);
-    transaction.burns = burns;
-    transaction.save();
-  }
-
-  // burn
-  if (event.params.to.toHex() == ADDRESS_ZERO && event.params.from.toHex() == pair.id) {
-    pair.totalSupply = pair.totalSupply.minus(value);
-    pair.save();
-
-    // this is a new instance of a logical burn
-    let burns = transaction.burns;
-    let burn: BurnEvent;
-    if (burns.length > 0) {
-      let currentBurn = BurnEvent.load(burns[burns.length - 1]);
-      if (currentBurn.needsComplete) {
-        burn = currentBurn as BurnEvent;
-      } else {
-        burn = new BurnEvent(
-          event.transaction.hash.toHex().concat("-").concat(BigInt.fromI32(burns.length).toString())
-        );
-        burn.transaction = transaction.id;
-        burn.needsComplete = false;
-        burn.pair = pair.id;
-        burn.liquidity = value;
-        burn.transaction = transaction.id;
-        burn.timestamp = transaction.timestamp;
-      }
-    } else {
-      burn = new BurnEvent(event.transaction.hash.toHex().concat("-").concat(BigInt.fromI32(burns.length).toString()));
-      burn.transaction = transaction.id;
-      burn.needsComplete = false;
-      burn.pair = pair.id;
-      burn.liquidity = value;
-      burn.transaction = transaction.id;
-      burn.timestamp = transaction.timestamp;
-    }
-
-    // if this logical burn included a fee mint, account for this
-    if (mints.length !== 0 && !isCompleteMint(mints[mints.length - 1])) {
-      let mint = MintEvent.load(mints[mints.length - 1]);
-      burn.feeTo = mint.to;
-      burn.feeLiquidity = mint.liquidity;
-      // remove the logical mint
-      store.remove("Mint", mints[mints.length - 1]);
-      // update the transaction
-
-      // TODO: Consider using .slice().pop() to protect against unintended
-      // side effects for other code paths.
-      mints.pop();
-      transaction.mints = mints;
-      transaction.save();
-    }
-    burn.save();
-    // if accessing last one, replace it
-    if (burn.needsComplete) {
-      // TODO: Consider using .slice(0, -1).concat() to protect against
-      // unintended side effects for other code paths.
-      burns[burns.length - 1] = burn.id;
-    }
-    // else add new one
-    else {
-      // TODO: Consider using .concat() for handling array updates to protect
-      // against unintended side effects for other code paths.
-      burns.push(burn.id);
-    }
-    transaction.burns = burns;
-    transaction.save();
-  }
-
-  transaction.save();
-}
+import { convertTokenToDecimal, FACTORY_ADDRESS, ONE_BI, ZERO_BD } from "./utils";
 
 export function handleSync(event: Sync): void {
   let pair = Pair.load(event.address.toHex());
@@ -230,111 +83,6 @@ export function handleSync(event: Sync): void {
   pancake.save();
   token0.save();
   token1.save();
-}
-
-export function handleMint(event: Mint): void {
-  let transaction = Transaction.load(event.transaction.hash.toHex());
-  let mints = transaction.mints;
-  let mint = MintEvent.load(mints[mints.length - 1]);
-
-  let pair = Pair.load(event.address.toHex());
-  let pancake = PancakeFactory.load(FACTORY_ADDRESS);
-
-  let token0 = Token.load(pair.token0);
-  let token1 = Token.load(pair.token1);
-
-  // update exchange info (except balances, sync will cover that)
-  let token0Amount = convertTokenToDecimal(event.params.amount0, token0.decimals);
-  let token1Amount = convertTokenToDecimal(event.params.amount1, token1.decimals);
-
-  // update txn counts
-  token0.totalTransactions = token0.totalTransactions.plus(ONE_BI);
-  token1.totalTransactions = token1.totalTransactions.plus(ONE_BI);
-
-  // get new amounts of USD and BNB for tracking
-  let bundle = Bundle.load("1");
-  let amountTotalUSD = token1.derivedBNB
-    .times(token1Amount)
-    .plus(token0.derivedBNB.times(token0Amount))
-    .times(bundle.bnbPrice);
-
-  // update txn counts
-  pair.totalTransactions = pair.totalTransactions.plus(ONE_BI);
-  pancake.totalTransactions = pancake.totalTransactions.plus(ONE_BI);
-
-  // save entities
-  token0.save();
-  token1.save();
-  pair.save();
-  pancake.save();
-
-  mint.sender = event.params.sender;
-  mint.amount0 = token0Amount as BigDecimal;
-  mint.amount1 = token1Amount as BigDecimal;
-  mint.logIndex = event.logIndex;
-  mint.amountUSD = amountTotalUSD as BigDecimal;
-  mint.save();
-
-  updatePairDayData(event);
-  updatePairHourData(event);
-  updatePancakeDayData(event);
-  updateTokenDayData(token0 as Token, event);
-  updateTokenDayData(token1 as Token, event);
-}
-
-export function handleBurn(event: Burn): void {
-  let transaction = Transaction.load(event.transaction.hash.toHex());
-  if (transaction === null) {
-    return;
-  }
-
-  let burns = transaction.burns;
-  let burn = BurnEvent.load(burns[burns.length - 1]);
-
-  let pair = Pair.load(event.address.toHex());
-  let pancake = PancakeFactory.load(FACTORY_ADDRESS);
-
-  //update token info
-  let token0 = Token.load(pair.token0);
-  let token1 = Token.load(pair.token1);
-  let token0Amount = convertTokenToDecimal(event.params.amount0, token0.decimals);
-  let token1Amount = convertTokenToDecimal(event.params.amount1, token1.decimals);
-
-  // update txn counts
-  token0.totalTransactions = token0.totalTransactions.plus(ONE_BI);
-  token1.totalTransactions = token1.totalTransactions.plus(ONE_BI);
-
-  // get new amounts of USD and BNB for tracking
-  let bundle = Bundle.load("1");
-  let amountTotalUSD = token1.derivedBNB
-    .times(token1Amount)
-    .plus(token0.derivedBNB.times(token0Amount))
-    .times(bundle.bnbPrice);
-
-  // update txn counts
-  pancake.totalTransactions = pancake.totalTransactions.plus(ONE_BI);
-  pair.totalTransactions = pair.totalTransactions.plus(ONE_BI);
-
-  // update global counter and save
-  token0.save();
-  token1.save();
-  pair.save();
-  pancake.save();
-
-  // update burn
-  // burn.sender = event.params.sender
-  burn.amount0 = token0Amount as BigDecimal;
-  burn.amount1 = token1Amount as BigDecimal;
-  // burn.to = event.params.to
-  burn.logIndex = event.logIndex;
-  burn.amountUSD = amountTotalUSD as BigDecimal;
-  burn.save();
-
-  updatePairDayData(event);
-  updatePairHourData(event);
-  updatePancakeDayData(event);
-  updateTokenDayData(token0 as Token, event);
-  updateTokenDayData(token1 as Token, event);
 }
 
 export function handleSwap(event: Swap): void {
@@ -416,7 +164,6 @@ export function handleSwap(event: Swap): void {
     transaction = new Transaction(event.transaction.hash.toHex());
     transaction.block = event.block.number;
     transaction.timestamp = event.block.timestamp;
-    transaction.mints = [];
     transaction.swaps = [];
     transaction.burns = [];
   }
